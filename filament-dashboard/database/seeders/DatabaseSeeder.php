a3fe4f104041a6919efab794957f471e6bd7aa60
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2025-03-13 22:36:51 +0330

    feat: :art: add location seeder

--- filament-dashboard/database/seeders/DatabaseSeeder.php
+++ filament-dashboard/database/seeders/DatabaseSeeder.php
@@ -13,5 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            CountrySeeder::class,
+            StateSeeder::class,
+            CitySeeder::class
+        ]);
     }
 }
